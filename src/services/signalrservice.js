5b9a9ba78abee2b87ec536540da9c322a303760d
yasin <tanriverdi.yasinn@gmail.com>
2026-02-02 15:52:47 +0300

    CANLIYA ÇIKIYORUZ

--- src/services/signalrservice.js
+++ src/services/signalrservice.js
@@ -1,53 +1,24 @@
-import { HubConnectionBuilder, HubConnectionState, LogLevel } from "@microsoft/signalr";
-
-// let connection = null;
-
-// export const startSignalRConnection = async (registerHandlers) => {
-//   if (connection && connection.state === HubConnectionState.Connected) {
-//     return connection;
-//   }
-
-//   const token = localStorage.getItem("jwtToken");
-
-//   connection = new HubConnectionBuilder()
-//     .withUrl("https://snowlybackend.onrender.com/snowlyHub", {
-//       accessTokenFactory: () => token,
-//       withCredentials: true
-//     })
-//     .withAutomaticReconnect()
-//     .configureLogging(LogLevel.Information)
-//     .build();
-
-//     connection.onclose((err) => console.log("SignalR bağlantısı kapandı:", err));
-
-
-//   await connection.start();
-
-//   registerHandlers(connection);
-
-
-
-//   return connection;
-// };
-
-// export const getConnection = () => connection;
+import {
+  HubConnectionBuilder,
+  HubConnectionState,
+  LogLevel
+} from "@microsoft/signalr";
 
 let connection = null;
 let handlersRegistered = false;
 
 export const startSignalRConnection = async (handlers) => {
-  if (connection && connection.state === "Connected") {
+  if (connection && connection.state === HubConnectionState.Connected) {
     return connection;
   }
 
   connection = new HubConnectionBuilder()
-    .withUrl("https://snowlybackend.onrender.com/snowlyHub", {
+    .withUrl(import.meta.env.VITE_SIGNALR_URL, {
       accessTokenFactory: () => localStorage.getItem("jwtToken")
     })
     .withAutomaticReconnect()
+    .configureLogging(LogLevel.Information)
     .build();
-
-  await connection.start();
 
   if (!handlersRegistered && handlers) {
     handlers(connection);
@@ -55,8 +26,11 @@ export const startSignalRConnection = async (handlers) => {
   }
 
   connection.onreconnected(() => {
-    handlers(connection);
+    // handler'lar zaten bağlı, tekrar ekleme
+    console.log("SignalR reconnected");
   });
+
+  await connection.start();
 
   return connection;
 };
